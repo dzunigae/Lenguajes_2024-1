78df44735baf10bb71a5a4e2c33f9d18228b035c
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2024-06-23 19:15:33 -0500

    Intento de la tarea 38

---
+++ Tarea 38/index.py
@@ -0,0 +1,14 @@
+# Si o si las cosas deben ser puestas en este orden
+C = [3,5] #Coeficientes
+Rs = [] #Declaración de la estructura que contiene las restricciones
+# Restricciones
+Rs.append(["<=",[1,2]]) 
+Rs.append(["=",[2,3]])
+B = [4,12] #Deben haber tantos valores como restricciones
+#Rango en el que se hará la operación
+r = []
+
+
+#Inicio de la ejecución principal
+print(Rs)
+#Final de la ejecución principal
